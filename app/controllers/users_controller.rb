d1e08a4cec1590b6f5df594c337344de9b2346a0
Mitsuki Matsui <2211140093u@ed.fuk.kindai.ac.jp>
2024-11-20 07:42:42 +0000

    modify_users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,26 @@
 class UsersController < ApplicationController
-  def index
-  end
-
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path, notice: 'アカウントが作成されました。'
+    else
+      render :new
+    end
   end
 
-  def edit
+  def show
+    @user = User.find(params[:id])
   end
 
-  def update
-  end
-  
-  def destroy
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
   end
   
 end

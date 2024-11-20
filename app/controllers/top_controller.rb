362d8eaf45cc95776579bc893ceb7deb7dbe4c23
Mitsuki Matsui <2211140093u@ed.fuk.kindai.ac.jp>
2024-11-20 07:46:07 +0000

    modify_top_controller

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,10 +1,23 @@
 class TopController < ApplicationController
   def main
+    # トップページの表示処理
   end
 
   def login
+    if request.post?
+      user = User.find_by(email: params[:email])
+      if user&.authenticate(params[:password])
+        session[:user_id] = user.id
+        redirect_to root_path, notice: 'ログインしました。'
+      else
+        flash.now[:alert] = 'メールアドレスまたはパスワードが間違っています。'
+        render :login
+      end
+    end
   end
 
   def logout
+    session[:user_id] = nil
+    redirect_to root_path, notice: 'ログアウトしました。'
   end
 end
